f50709daaf81382f2a3f427f762e41a911647a2b
Aaron Williams <aaron.colby.williams@gmail.com>
2015-06-09 14:58:30 -0700

    added xmljson lib and created dict structure to export to

--- parse.py
+++ parse.py
@@ -5,8 +5,10 @@ import zipfile
 
 import xmltodict
 from lxml import etree
-from lib.utils import all_files, unzip
 from ipdb import set_trace as debugger
+
+from lib.xml2json import etree_to_dict
+from lib.utils import all_files, unzip
 
 DATA_DIR = os.path.join(os.getcwd(), 'data')
 DUMP_DIR = os.path.join(os.getcwd(), 'dump')
@@ -53,6 +55,17 @@ def _remove_namespaces(root_element):
 
     return transform(root_element)
 
+def elem2dict(node):
+    """
+    Convert an lxml.etree node tree into a dict.
+    """
+    d = {}
+    for e in node.iterchildren():
+        key = e.tag.split('}')[1] if '}' in e.tag else e.tag
+        value = e.text if e.text else elem2dict(e)
+        d[key] = value
+    return d
+
 def unzip_data():
     """
     Unzip newsgate zip export and exact data to DUMP_DIR
@@ -72,15 +85,34 @@ def transform_xml_to_json():
     """
     xml_files = list(all_files(DUMP_DIR, '*.xml'))
 
+    jointext = lambda alist: ''.join([el.text.encode('utf-8') for el in alist])
+
     for xf in xml_files:
         with open(xf, 'rb') as xmlfile:
             raw_xml = xmlfile.read()
             root = etree.fromstring(raw_xml)
             dom = _remove_namespaces(root)
 
-            # dom.xpath('//ccitext/*/*/text()')  # Almost
+            hed = dom.xpath('//mm_head/p')
+            subhed = dom.xpath('//head/p')
+            byline = dom.xpath('//byline_name/descendant-or-self::*')
 
-            content = xmltodict.parse(raw_xml, process_namespaces=True)
+            #  endnote_contact just has a bio so let's ignore that
+            # article = dom.xpath('//body/*[not(self::endnote_contact)]')
+            article = dom.xpath('//body/descendant::*/text()')
+
+            # create a list of sentences from the document where. We'll break
+            # on newlines and store the result in a dict
+            content = {
+                "hed": jointext(hed),
+                'subhed': jointext(subhed),
+                'byline': jointext(byline),
+                'article': "".join([p.encode('utf-8') for p in article])\
+                    .strip().split('\n')
+            }
+
+            debugger()
+
             output_json = json.dumps(content)
             filename = "{}.json".format(os.path.split(xmlfile.name)[1])
 
